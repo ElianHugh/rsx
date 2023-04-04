9f94a72089aabc408e803290a8703709d6963c43
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2023-04-04 18:23:56 +1000

    Add failing test

--- tests/testthat/test_4-component-styles.R
+++ tests/testthat/test_4-component-styles.R
@@ -73,3 +73,29 @@ test_that("styles should be overwritten with same-named components", {
         expect_true(grepl("color: blue", styles))
     })
 })
+
+test_that("styles are compiled even when component is created from a method", {
+    suppressMessages({
+        reset_rsx_env()
+        x <- component(
+            name = "x",
+            template = function(ns) {
+                self$new_y()
+            },
+            methods = list(
+                new_y = function() {
+                    y()
+                }
+            )
+        )
+        y <- component(
+            name = "y",
+            template = function(ns) {
+                "Hello world"
+            },
+            styles = "* { color: blue }"
+        )
+        x()
+        expect_true(grepl("color: blue", aggregate_styles()))
+    })
+})
