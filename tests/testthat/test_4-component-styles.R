c5be05729caa53c3564ce71093198c4b89d52260
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2023-04-05 12:43:57 +1000

    Another failing test

--- tests/testthat/test_4-component-styles.R
+++ tests/testthat/test_4-component-styles.R
@@ -74,6 +74,27 @@ test_that("styles should be overwritten with same-named components", {
     })
 })
 
+test_that("styles are compiled for each used component", {
+    suppressMessages({
+        reset_rsx_env()
+        x <- component(
+            name = "x",
+            template = function(ns) {
+                y()
+            }
+        )
+        y <- component(
+            name = "y",
+            template = function(ns) {
+                "Hello world"
+            },
+            styles = "* { color: blue }"
+        )
+        x()
+        expect_true(grepl("color: blue", aggregate_styles()))
+    })
+})
+
 test_that("styles are compiled even when component is created from a method", {
     suppressMessages({
         reset_rsx_env()
