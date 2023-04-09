dfb5a39acd4a57f65f8185db8ea589ddae9f7b09
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2023-04-09 20:44:22 +1000

    Styles is a function

--- R/component-styles.R
+++ R/component-styles.R
@@ -2,7 +2,7 @@
 #'
 #' @description
 #'
-#' The styles argument is a character vector of length 1 that defines the styles for the component.
+#' The styles argument is function that returns a character vector or list that defines the styles for the component.
 #' Styles are scoped to the component.
 #'
 #' @details
@@ -11,7 +11,9 @@
 #' For example:
 #'
 #' ```r
-#' styles = "a { color: red; }"
+#' styles = function() {
+#' "a { color: red; }"
+#' }
 #' ````
 #'
 #' This would define the CSS styles for the anchor elements (`<a>`) in the component, setting their color to red.
@@ -25,13 +27,9 @@
 NULL
 
 
-new_styles_property <- function(styles, hash) {
-    if (!is.null(styles)) {
-        if (!is.character(styles)) {
-            error_component_invalid_styles()
-        }
-        sprintf(".%s { %s }", hash, styles) |>
-            sass::sass()
+new_styles_property <- function(style_fun, hash) {
+    if (!is.null(style_fun)) {
+        generate_scoped_sass(style_fun, hash)
     } else {
         NULL
     }
@@ -40,7 +38,16 @@ new_styles_property <- function(styles, hash) {
 validate_styles_property <- function(styles) {
     if (!is.null(styles)) {
         if (!is.character(styles)) {
-            return("- $styles must be a character vector of length 1")
+            return("- $styles must be a function that returns a character vector or list")
         }
     }
+}
+
+generate_scoped_sass <- function(style_fn, hash) {
+    styles <- style_fn()
+    if (is.list(styles)) {
+        styles <- paste(styles, collapse = "\n", sep = "\n")
+    }
+    sprintf(".%s { %s }", hash, styles) |>
+        sass::sass()
 }
