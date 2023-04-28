80c7946af74d5170967b1c8cdba32deb962fd06a
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2023-04-28 11:45:11 +1000

    refactor

--- R/instance-core.R
+++ R/instance-core.R
@@ -31,22 +31,9 @@ new_instance <- function(comp) {
     envs <- create_environments(comp)
     this <- create_env_bindings(comp, envs)
 
-    inst_methods <- instantiate_functions(attr(comp, "methods"), this)
-    inst_data <- instantiate_data(attr(comp, "data"), this)
-    inst_data_nms <- names(inst_data)
-    inst_method_nms <- names(inst_methods)
+    instantiate_data_property(comp, envs)
+    instantiate_methods_property(comp, envs)
 
-    if (assert_no_name_duplication(inst_data_nms, inst_method_nms)) {
-        nms <- c(inst_data_nms, inst_method_nms)
-        error_instance_name_duplication(nms[duplicated(nms)], this)
-    }
-
-    validation_msg <- validate_params(inst_data, inst_methods)
-    if (!is.null(validation_msg)) {
-        error_instance_validation(validation_msg, this$component)
-    }
-
-    list2env(c(inst_data, inst_methods), this$internal$self)
     lockEnvironment(this$internal)
     lockEnvironment(this$internal[["self"]])
 
@@ -66,33 +53,11 @@ create_environments <- function(comp) {
 }
 
 create_env_bindings <- function(comp, envs) {
-
-
     envs$inst_env$instance_id <- sprintf("instance-%s", random_id())
     envs$inst_env$component <- comp
     envs$inst_env$internal <- envs$internal_env
     envs$inst_env$template <- instantiate_template(comp$template, envs$internal_env)
-
     envs$internal_env$self <- envs$self_env
-
-    data_getter <- function() {
-        get_data(envs$self_env)
-    }
-
-    data_setter <- function(v) {
-        error_instance_assignment(envs$inst_env, "data")
-    }
-
-    methods_getter <- function() {
-        get_functions(envs$self_env)
-    }
-
-    methods_setter <- function(v) {
-        error_instance_assignment(envs$inst_env, "methods")
-    }
-
-    new_active_binding("data", getter = data_getter, setter = data_setter, env = envs$inst_env)
-    new_active_binding("methods", getter = methods_getter, setter = methods_setter, env = envs$inst_env)
 
     envs$inst_env
 }
@@ -107,15 +72,6 @@ validate_params <- function(dat, methods) {
         )
     }
     NULL
-}
-
-instantiate_functions <- function(methods, inst) {
-    lapply(methods, set_fn_env, inst$internal)
-}
-
-instantiate_data <- function(dat, inst) {
-    environment(dat) <- inst[["internal"]]
-    invisible(dat())
 }
 
 instantiate_template <- function(tmp, env) {
